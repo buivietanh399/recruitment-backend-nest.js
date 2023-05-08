897a200b019a075eb5e1c72e5474df9fc8e49771
Viet Anh <buivietanh399@gmail.com>
2023-05-08 20:45:00 +0700

    login jwt complete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { Public } from './decorator/customize';
 
 @Controller() //  route /
 export class AppController {
@@ -13,11 +14,24 @@ export class AppController {
 
   ) { }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
   }
 
+  // @UseGuards(JwtAuthGuard)
+  @Public()
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile1')
+  getProfile1(@Request() req) {
+    return req.user;
+  }
 
 }
