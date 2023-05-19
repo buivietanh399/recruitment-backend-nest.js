fe8fd3bdcbcc6d168321f2c127b71149cc1fb63e
Viet Anh <buivietanh399@gmail.com>
2023-05-19 20:57:00 +0700

    more token and api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,17 @@ import {
   Controller,
   Post,
   UseGuards,
-  Request,
   Get,
   Body,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from '@/decorator/customize';
+import { Public, ResponseMessage, User } from '@/decorator/customize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { RegisterUserDto } from '@/users/dto/create-user.dto';
-
+import { Request, Response } from 'express';
+import { IUser } from '@/users/users.interface';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -18,8 +20,9 @@ export class AuthController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  handleLogin(@Request() req) {
-    return this.authService.login(req.user);
+  @ResponseMessage('User login')
+  handleLogin(@Req() req, @Res({ passthrough: true }) response: Response) {
+    return this.authService.login(req.user, response);
   }
 
   // @UseGuards(JwtAuthGuard)
@@ -28,5 +31,31 @@ export class AuthController {
   @Post('/register')
   handleRegister(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
+  }
+
+  @ResponseMessage('Get user information')
+  @Get('/account')
+  handleGetAcount(@User() user: IUser) {
+    return { user };
+  }
+
+  @Public()
+  @ResponseMessage('Get user by refresh token')
+  @Get('/refresh')
+  handleRefreshToken(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const refreshToken = request.cookies['refresh_token'];
+    return this.authService.processNewToken(refreshToken, response);
+  }
+
+  @ResponseMessage('Logout User')
+  @Post('logout')
+  handleLogout(
+    @Res({ passthrough: true }) response: Response,
+    @User() user: IUser,
+  ) {
+    return this.authService.logout(response, user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from '@/users/users.interface';
 import { RegisterUserDto } from '@/users/dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
+import { Response, response } from 'express';
+import cookieParser from 'cookie-parser';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
   //ussername/ pass là 2 tham số thư viện passport nó ném về
@@ -24,7 +29,7 @@ export class AuthService {
     return null;
   }
 
-  async login(user: IUser) {
+  async login(user: IUser, response: Response) {
     const { _id, name, email, role } = user;
     const payload = {
       sub: 'token login',
@@ -34,12 +39,27 @@ export class AuthService {
       email,
       role,
     };
+
+    const refresh_token = this.createRefreshToken(payload);
+
+    //update user with refresh token
+    await this.usersService.updateUserToken(refresh_token, _id);
+
+    //set refresh token as cookies
+    response.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      maxAge: ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) * 1000,
+    });
+
     return {
       access_token: this.jwtService.sign(payload),
-      _id,
-      name,
-      email,
-      role,
+
+      user: {
+        _id,
+        name,
+        email,
+        role,
+      },
     };
   }
 
@@ -50,4 +70,68 @@ export class AuthService {
       createdAt: newUser?.createdAt,
     };
   }
+
+  createRefreshToken = (payload: any) => {
+    const refresh_token = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+      expiresIn:
+        ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) / 1000,
+    });
+
+    return refresh_token;
+  };
+
+  processNewToken = async (refreshToken: string, response: Response) => {
+    try {
+      this.jwtService.verify(refreshToken, {
+        secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+      });
+
+      //todo
+      let user = await this.usersService.findUserByToken(refreshToken);
+      if (user) {
+        const { _id, name, email, role } = user;
+        const payload = {
+          sub: 'token refresh',
+          iss: 'from server',
+          _id,
+          name,
+          email,
+          role,
+        };
+
+        const refresh_token = this.createRefreshToken(payload);
+        // update user with refresh token
+        await this.usersService.updateUserToken(refresh_token, _id.toString());
+
+        //set refresh token as cookies
+        response.clearCookie('refresh_token');
+
+        response.cookie('refresh_token', refresh_token, {
+          httpOnly: true,
+          maxAge:
+            ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) * 1000,
+        });
+
+        return {
+          access_token: this.jwtService.sign(payload),
+          user: { _id, name, email, role },
+        };
+      } else {
+        throw new BadRequestException(
+          'refresh token không hợp lệ. Vui lòng login',
+        );
+      }
+    } catch (error) {
+      throw new BadRequestException(
+        'Refresh token không hợp lệ. Vui lòng login lại',
+      );
+    }
+  };
+
+  logout = async (response: Response, user: IUser) => {
+    await this.usersService.updateUserToken('', user._id);
+    response.clearCookie('refresh_token');
+    return 'OK';
+  };
 }
